60308e06a3ba1f231a55f6386777d6f43d002390
Henry (Local) <henryjiang@live.ca>
2019-03-02 16:46:15 -0800

    Connection w/ PHP/MySQL in Android

--- SMPLogin/app/src/main/java/ca/bcit/smp/MainActivity.java
+++ SMPLogin/app/src/main/java/ca/bcit/smp/MainActivity.java
@@ -2,12 +2,32 @@ package ca.bcit.smp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameField,passwordField;
+    private TextView status,role;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameField = (EditText)findViewById(R.id.usernameEditText);
+        passwordField = (EditText)findViewById(R.id.passwordEditText);
+
+        status = (TextView)findViewById(R.id.textView6);
+        role = (TextView)findViewById(R.id.textView7);
+    }
+
+
+    public void login(View view){
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+        new SigninActivity(this,status,role,0).execute(username,password);
+
     }
 }
