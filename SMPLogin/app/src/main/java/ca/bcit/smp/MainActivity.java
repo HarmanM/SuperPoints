edd28cb93b17ca9268f4ba60288f240a0e9edf44
Unknown <harman_minhas@outlook.com>
2019-03-05 18:47:07 -0800

    added test page to see if user info is retained

--- SMPLogin/app/src/main/java/ca/bcit/smp/MainActivity.java
+++ SMPLogin/app/src/main/java/ca/bcit/smp/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.bcit.smp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
         new SigninActivity(this,status,role,0).execute(username,password);
+        Intent i = new Intent(MainActivity.this, TestActivity.class);
+        startActivity(i);
 
     }
 }
