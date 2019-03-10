6cefbae21dac10a38523552a8f6edd6d9ea2f6fc
Oliver Jang <jangoliver@gmail.com>
2019-03-10 11:43:00 -0700

    Revert "Revert "Revert "Revert "Added preliminary beacon code""""
    This reverts commit 39356bdb90fb163771128917c46a775284e317db.

--- SMPv2/app/src/main/java/ca/bcit/smpv2/MainActivity.java
+++ SMPv2/app/src/main/java/ca/bcit/smpv2/MainActivity.java
@@ -1,10 +1,16 @@
 package ca.bcit.smpv2;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 
 import com.google.android.gms.maps.MapView;
@@ -13,11 +19,36 @@ import com.google.android.gms.maps.MapView;
 public class MainActivity extends AppCompatActivity {
 
     private MapView mMapView;
+    protected static final String TAG = "MainActivity";
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+    BeaconRanger br;
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.mMapView = new MapView(this);
+
+        br = new BeaconRanger(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+
+
+
         /* Now delegate the event to the MapView. */
         this.mMapView.onCreate(savedInstanceState);
 
@@ -51,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
     public final void onDestroy() {
         super.onDestroy();
         this.getMapView().onDestroy();
+        br.onDestroy();
     }
 
     @Override
@@ -77,5 +109,30 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
     }
 }
