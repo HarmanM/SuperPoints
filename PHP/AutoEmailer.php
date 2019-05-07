fb5ea858cbe9cb3ee6518ee6da73808932715f00
Harman Minhas <harman_minhas@outlook.com>
2019-05-07 12:16:24 -0700

    more work on autoemailer

--- PHP/AutoEmailer.php
+++ PHP/AutoEmailer.php
@@ -7,6 +7,21 @@ require 'PHPMailer/PHPMailer/src/Exception.php';
 require 'PHPMailer/PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/PHPMailer/src/SMTP.php';
 
+define('DB_SERVER', "superpointsdb.cuwiyoumlele.ca-central-1.rds.amazonaws.com");
+define('DB_USERNAME', "admin");
+define('DB_PASSWORD', "zxcasdqwe123");
+
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+if (mysqli_connect_errno($con)) {
+    echo "Failed to connect to database: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT value FROM superpoints.UserSettings WHERE userID = 0", MYSQLI_STORE_RESULT);
+$row = mysqli_fetch_array($result);
+$sendMonthly = $row['value'];
+
+
 $mail = new PHPMailer();
 $mail->isSMTP();
 $mail->SMTPAuth = true;
@@ -21,12 +36,15 @@ $mail->Subject = 'Monthly KPI Report';
 $mail->Body = 'Test';
 $mail->AddAddress('spemaileradm@gmail.com');
 
-if(!$mail->send()) {
-  echo 'Message was not sent.';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
+if ($sendMonthly) {
+  if(!$mail->send()) {
+    echo 'Message was not sent.';
+    echo 'Mailer error: ' . $mail->ErrorInfo;
+  } else {
+    echo 'Message has been sent.';
+  }
 }
+
 
 
 
