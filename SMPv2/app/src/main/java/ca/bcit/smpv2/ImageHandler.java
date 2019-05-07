d4321a1cc4bea7dc6093bb949a6f3b01cc1ef494
Harman Minhas <harman_minhas@outlook.com>
2019-05-07 10:43:31 -0700

    fixing promotion image error (adding a new promo without image would use the last promo image)

--- SMPv2/app/src/main/java/ca/bcit/smpv2/ImageHandler.java
+++ SMPv2/app/src/main/java/ca/bcit/smpv2/ImageHandler.java
@@ -1,4 +1,5 @@
 package ca.bcit.smpv2;
+
 import android.content.ContentResolver;
 import android.content.Context;
 import android.net.Uri;
@@ -25,27 +26,27 @@ import java.io.OutputStream;
 
 public class ImageHandler {
 
-	private static ImageHandler instance;
-	private BasicAWSCredentials credentials = new BasicAWSCredentials("AKIATGLPVNHQC5LDNPX2", "zx9MVKsBBhBlnaTikDvHYWbXLbTrNgdETnF3eWOX");
-	private AmazonS3Client s3Client = new AmazonS3Client(credentials);
-	private ImageHandler() {
-	}
+    private static ImageHandler instance;
+    private BasicAWSCredentials credentials = new BasicAWSCredentials("AKIATGLPVNHQC5LDNPX2", "zx9MVKsBBhBlnaTikDvHYWbXLbTrNgdETnF3eWOX");
+    private AmazonS3Client s3Client = new AmazonS3Client(credentials);
 
-	public static ImageHandler getInstance() {
-		if (instance != null) {
-			return instance;
-		} else {
-			instance = new ImageHandler();
-			return instance;
-		}
-	}
+    private ImageHandler() {
+    }
 
-
-	public void uploadFile(Uri fileUri, String name, Context context, Runnable onCompleteFunc) {
+    public static ImageHandler getInstance() {
+        if (instance != null) {
+            return instance;
+        } else {
+            instance = new ImageHandler();
+            return instance;
+        }
+    }
 
 
-		if (fileUri != null) {
-			final String fileName = name;
+    public void uploadFile(Uri fileUri, String name, Context context, Runnable onCompleteFunc) {
+
+        if (fileUri != null) {
+            final String fileName = name;
 
 			/*
 			if (!validateInputFileName(fileName)) {
@@ -53,40 +54,40 @@ public class ImageHandler {
 			}
 			*/
 
-			final File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-					"/" + fileName);
+            final File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    "/" + fileName);
 
-			createFile(context, fileUri, file);
+            createFile(context, fileUri, file);
 
-			TransferUtility transferUtility =
-					TransferUtility.builder()
-							.context(context)
-							.awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
-							.s3Client(s3Client)
-							.build();
+            TransferUtility transferUtility =
+                    TransferUtility.builder()
+                            .context(context)
+                            .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                            .s3Client(s3Client)
+                            .build();
 
-			ObjectMetadata meta = new ObjectMetadata();
-			meta.setContentType("image/png");
+            ObjectMetadata meta = new ObjectMetadata();
+            meta.setContentType("image/png");
 
-			TransferObserver uploadObserver =
-					transferUtility.upload("promo/" + fileName + ".jpg", file, meta);
-			uploadObserver.setTransferListener(new TransferListener() {
-				public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
-				}
+            TransferObserver uploadObserver =
+                    transferUtility.upload("promo/" + fileName + ".jpg", file, meta);
+            uploadObserver.setTransferListener(new TransferListener() {
+                public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                }
 
-				public void onStateChanged(int id, TransferState state) {
-					if(state == TransferState.COMPLETED)
-						onCompleteFunc.run();
-				}
+                public void onStateChanged(int id, TransferState state) {
+                    if (state == TransferState.COMPLETED)
+                        onCompleteFunc.run();
+                }
 
-				public void onError(int id, Exception ex) {
-				}
-			});
+                public void onError(int id, Exception ex) {
+                }
+            });
 
-		}
-	}
+        }
+    }
 
-	public void deleteImage(String promoID, Context context) {
+    public void deleteImage(String promoID, Context context) {
         TransferUtility transferUtility =
                 TransferUtility.builder()
                         .context(context)
@@ -96,16 +97,16 @@ public class ImageHandler {
         s3Client.deleteObject("superpoints-userfiles-mobilehub-467637819", "promo/" + promoID + ".jpg");
     }
 
-	private void createFile(Context context, Uri srcUri, File dstFile) {
-		try {
-			InputStream inputStream = context.getContentResolver().openInputStream(srcUri);
-			if (inputStream == null) return;
-			OutputStream outputStream = new FileOutputStream(dstFile);
-			IOUtils.copy(inputStream, outputStream);
-			inputStream.close();
-			outputStream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private void createFile(Context context, Uri srcUri, File dstFile) {
+        try {
+            InputStream inputStream = context.getContentResolver().openInputStream(srcUri);
+            if (inputStream == null) return;
+            OutputStream outputStream = new FileOutputStream(dstFile);
+            IOUtils.copy(inputStream, outputStream);
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
