d2426e9e19365ced49b067912a595784521db807
Harman Minhas <harman_minhas@outlook.com>
2019-05-13 15:51:08 -0700

    seekbar page also shows current points

--- SMPv2/app/src/main/java/ca/bcit/smpv2/PointsAdapter.java
+++ SMPv2/app/src/main/java/ca/bcit/smpv2/PointsAdapter.java
@@ -43,7 +43,7 @@ public class PointsAdapter extends ArrayAdapter<Pair<Business, Points>> {
         if(curTier != UserPointsActivity.tiers.size() - 1) {
             seekBar.setProgress((int) ((float) (userPoints.second.getPoints() - UserPointsActivity.tiers.get(curTier).getMinPoints())
                     / (UserPointsActivity.tiers.get(curTier + 1).getMinPoints() - UserPointsActivity.tiers.get(curTier).getMinPoints()) * 100));
-            ((TextView) convertView.findViewById(R.id.pointsTextView)).setText(String.valueOf((float) seekBar.getProgress() / 100 * UserPointsActivity.tiers.get(curTier).getMinPoints()));
+            ((TextView) convertView.findViewById(R.id.pointsTextView)).setText(String.valueOf((float) seekBar.getProgress() / 100 * UserPointsActivity.tiers.get(curTier + 1).getMinPoints()));
 
             ((TextView) convertView.findViewById(R.id.curTier)).setText(UserPointsActivity.tiers.get(curTier).getName() + " (" + UserPointsActivity.tiers.get(curTier).getMinPoints() + ")");
             ((TextView) convertView.findViewById(R.id.nextTier)).setText(UserPointsActivity.tiers.get(curTier + 1).getName() + " (" + UserPointsActivity.tiers.get(curTier + 1).getMinPoints() + ")");
